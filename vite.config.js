feb42de6d312f1b81e8b205bf4021220501e0086
soundwanders <soundwanders@gmail.com>
2025-07-13 23:47:14 -0400

    setup railway env

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,19 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: './src/client',
   server: {
-    port: 3000, 
-    open: true, 
+    port: 3000,
+    open: true,
   },
   build: {
     outDir: './dist',
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['@supabase/supabase-js']
+        }
+      }
+    }
   },
   define: {
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
